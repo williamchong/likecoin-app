b6ffecc6fe988b12c83f844fa43723e3b209486c
Ng Wing Tat, David <i@ngwingt.at>
2019-08-16 15:16:03 +0800

    🎉 Initial commit

---
+++ LikeCoin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LikeCoin
+//
+//  Created by David Ng on 7/8/2019.
+//  Copyright © 2019 LikeCoin Foundation Limited. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
